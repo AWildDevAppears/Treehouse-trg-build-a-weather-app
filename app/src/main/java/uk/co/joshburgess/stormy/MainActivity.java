cf79877b9a858de25da34adad5de2d9d28431cee
woJburgess <jburgess@whiteoctober.co.uk>
2016-06-29 16:49:37 +0100

    What To Do When the Network is Down

--- app/src/main/java/uk/co/joshburgess/stormy/MainActivity.java
+++ app/src/main/java/uk/co/joshburgess/stormy/MainActivity.java
@@ -1,9 +1,13 @@
 package uk.co.joshburgess.stormy;
 
 import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private double lati = 37.8267;
     private double longi = -122.423;
+    private String forecastURL = "https://api.forecast.io/forecast/" + API_KEY + "/" + lati + "," + longi;
+
 
     private OkHttpClient client;
 
@@ -26,31 +32,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String forecastURL = "https://api.forecast.io/forecast/" + API_KEY + "/" + lati + "," + longi;
+        if (isNetworkAvaiable()) {
+            client = new OkHttpClient();
 
-        client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
 
-        Request request = new Request.Builder()
-                .url(forecastURL)
-                .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    if (response.isSuccessful()) {
-                    }
-                } catch (IOException e) {
-                    alertUserOfError();
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserOfError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error: ", e);
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, getString(R.string.network_unavailable_text), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvaiable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnected());
     }
 
     private void alertUserOfError() {
